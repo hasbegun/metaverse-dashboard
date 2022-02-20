6ecbde38514a7a39bd1d1e1c541034826a057404
Inho Choi <inho.choi@menlosecurity.com>
2022-02-20 15:56:52 -0800

    Fix for nginx

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
 
 IMAGE_NAME=metaverse-dashboard
+# change repo and branch for debug and dev.
 REPO=hasbegun
 BRANCH=dev_ssl
 COMMIT=$(git rev-parse --short HEAD)
-docker build --build-arg=COMMIT=$(git rev-parse --short HEAD) \
+echo "Build $IMAGE_NAME based on $COMMIT from $REPO/$BRANCH."
+docker build --build-arg=COMMIT=$COMMIT \
     --build-arg=REPO=$REPO \
     --build-arg=BRANCH=$BRANCH \
     -t $IMAGE_NAME -f Dockerfile .
