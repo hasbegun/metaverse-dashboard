a8688753c187cd15fa25641a9eb46d236eb33c87
Inho Choi <inho.choi@menlosecurity.com>
2022-02-20 16:00:21 -0800

    change comment

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,7 @@ module.exports = {
         devServer: {
             headers: { 'Access-Control-Allow-Origin': '*' },
             //-- SECURITY --//
-            // Do not set this flag to resolce NGINX "invalid host request" error.
+            // Do not set this flag to resolve the "invalid host request" error.
             // disableHostCheck: true
             /////////////////
             public: 'dashboard.innoxai.com:8088'
