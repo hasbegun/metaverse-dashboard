98f494eb8296a5cbd00a24d5814bfb928ee09809
Inho Choi <inho.choi@menlosecurity.com>
2022-02-20 22:17:25 -0800

    change url

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -5,7 +5,7 @@ IMAGE_NAME=metaverse-dashboard
 REPO=hasbegun
 BRANCH=dev_ssl
 COMMIT=$(git rev-parse --short HEAD)
-echo "Build $IMAGE_NAME based on $COMMIT from $REPO/$BRANCH."
+echo "Build $IMAGE_NAME based on $COMMIT from $REPO:$BRANCH."
 docker build --build-arg=COMMIT=$COMMIT \
     --build-arg=REPO=$REPO \
     --build-arg=BRANCH=$BRANCH \
