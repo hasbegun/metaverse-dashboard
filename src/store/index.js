ff03b1b388f4949275d119b72ccc819c5e2db925
Inho Choi <inho.choi@menlosecurity.com>
2022-03-16 06:52:32 -0700

    reset metaverse server

--- src/store/index.js
+++ src/store/index.js
@@ -33,7 +33,8 @@ export default new Vuex.Store({
             nickname: '',
             // server: 'https://metaverse.bluestuff.org:9400', // This needs to at least be pre-filled in order to get all other config information.
             // server: 'https://metaverse.vircadia.com/live', // This needs to at least be pre-filled in order to get all other config information.
-            server: 'vircadia-metaverse-server:9400', // docker-compose
+            // server: 'vircadia-metaverse-server:9400', // docker-compose
+            server: 'https://metaverse.innoxai.com', // docker-compose
             iceServer: '',
             serverVersion: ''
         },
